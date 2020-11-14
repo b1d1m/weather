bfd402af8d603c8be12e950a8b1a0af6270d85e5
Mohammad Abdullah <54266182+b1d1m@users.noreply.github.com>
2020-11-14 20:19:39 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = [];
+const express =  require('express');
+const bodyParser = require('body-parser');
+// Require Express to run server and routes
+const app = express();
+const PORT = '4040';
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+app.get('/get', function(req, res) {
+    res.send(projectData);
+})
+
+app.post('/', addValues);
+
+function addValues(req,res) {
+    projectData.push(req.body);
+}
+
+
+// Setup Server
+const server = app.listen(PORT, () => { console.log(`Server is working on port: ${PORT} `)});
