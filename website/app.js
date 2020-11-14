5317172a2638088ba03f6a1bea6e5d871c490d78
Mohammad Abdullah <54266182+b1d1m@users.noreply.github.com>
2020-11-14 20:23:09 +0300

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,1 +1,63 @@
+/* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let apiKey = '&appid=33a289cc57cd0fcb3794e23acff93d3d';
 
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', queryWeather);
+
+function queryWeather(e) {
+    const zipCode = document.getElementById('zip').value;
+    let feelings = document.getElementById('feelings').value;
+    getWeather(baseURL,zipCode,apiKey)
+    .then (function(data) {
+       postWeather('/', {temprature: data.main.temp, date: newDate, userResponse: feelings} );
+    })
+    .then (
+        updateUI('/get')
+    );
+};
+
+const getWeather = async (baseURL, zip, apiKey) => {
+    const res = await fetch(baseURL+zip+apiKey)
+    try{
+        const data = res.json();
+        return data;
+    }catch(error) {
+        console.log('error', error);
+    }
+}
+
+const postWeather = async ( url = '', data = {}) => {
+    const response = await fetch(url,  {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify(data), 
+      });
+
+        try {
+            const newData = await response.json();
+            return newData;
+        }catch(error) {
+        console.log("error", error);
+        }
+}
+
+const updateUI = async (url='') => {
+    console.log(url);
+    const request = await fetch(url);
+    try {
+        const allData = await request.json();
+        console.log(allData)
+        document.getElementById('date').innerHTML = allData[0].date;
+        document.getElementById('temp').innerHTML =  allData[0].temprature;
+        document.getElementById('content').innerHTML = allData[0].userResponse;
+    }catch(error) {
+        console.log("error", error);
+    }
+}
